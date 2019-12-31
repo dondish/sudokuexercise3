7cefe89a24f1634d893482ab93cd56fa7d10b686
dondish <odedshapira1@gmail.com>
2019-12-31 13:50:52 +0200

    finished parser

--- parser.c
+++ parser.c
@@ -3,25 +3,29 @@
 #include<string.h>
 
 int parseLine(const char *line, command_t *cmd) {
-    if (sscanf(line, "set %d %d %d", &cmd->X, &cmd->Y, &cmd->Z) == 3) {
+    char buf[9];
+    if (sscanf(line, " set %d %d %d", &cmd->X, &cmd->Y, &cmd->Z) == 3) {
         cmd->type = CMDTYPE_SET;
         return 1;
     }
-    if (sscanf(line, "hint %d %d", &cmd->X, &cmd->Y) == 2) {
+    if (sscanf(line, " hint %d %d", &cmd->X, &cmd->Y) == 2) {
         cmd->type = CMDTYPE_HINT;
         return 1;
     }
-    if (!strncmp("validate", line, 8)) {
-        cmd->type = CMDTYPE_VALIDATE;
-        return 1;
+    if (sscanf(line, " %8s", buf) == 1) {
+        if (!strcmp("validate", buf)) {
+            cmd->type = CMDTYPE_VALIDATE;
+            return 1;
+        }
+        if (!strcmp("restart", buf)) {
+            cmd->type = CMDTYPE_RESTART;
+            return 1;
+        }
+        if (!strcmp("exit", buf)) {
+            cmd->type = CMDTYPE_EXIT;
+            return 1;
+        }
     }
-    if (!strncmp("restart", line, 7)) {
-        cmd->type = CMDTYPE_RESTART;
-        return 1;
-    }
-    if (!strncmp("exit", line, 4)) {
-        cmd->type = CMDTYPE_EXIT;
-        return 1;
-    }
+    
     return 0;
 }
--- parser_test.c
+++ parser_test.c
@@ -4,6 +4,9 @@
 int main()
 {
     command_t cmd;
+
+    /* basic tests */
+
     const char *line = "set 1 2 3";
     assert(parseLine(line, &cmd));
     assert(cmd.type == CMDTYPE_SET);
@@ -31,6 +34,51 @@ int main()
     assert(cmd.type == CMDTYPE_EXIT);
 
     line = "fake";
+    assert(!parseLine(line, &cmd));
+
+    /* initial space test */
+
+    line = "  hint 4 5";
+    assert(parseLine(line, &cmd));
+    assert(cmd.type == CMDTYPE_HINT);
+    assert(cmd.X == 4);
+    assert(cmd.Y == 5);
+
+    line = "  validate";
+    assert(parseLine(line, &cmd));
+    assert(cmd.type == CMDTYPE_VALIDATE);
+
+    /* additional parameters */
+    
+    line = "set 1 2 3 4";
+    assert(parseLine(line, &cmd));
+    assert(cmd.type == CMDTYPE_SET);
+    assert(cmd.X == 1);
+    assert(cmd.Y == 2);
+    assert(cmd.Z == 3);
+
+    line = "set 1 2 3 yeetus";
+    assert(parseLine(line, &cmd));
+    assert(cmd.type == CMDTYPE_SET);
+    assert(cmd.X == 1);
+    assert(cmd.Y == 2);
+    assert(cmd.Z == 3);
+
+    /* insufficient parameters */
+
+    line = "set 1 2";
+    assert(!parseLine(line, &cmd));
+
+    line = "set 1";
+    assert(!parseLine(line, &cmd));
+
+    line = "set";
+    assert(!parseLine(line, &cmd));
+
+    line = "hint 1";
+    assert(!parseLine(line, &cmd));
+
+    line = "hint";
     assert(!parseLine(line, &cmd));
 
     return 0;
