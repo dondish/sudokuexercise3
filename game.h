d540dcba454950ae0ad25fd0dbbbc997e0bedc4d
dondish <odedshapira1@gmail.com>
2019-12-31 13:58:36 +0200

    start of game.h

--- game.h
+++ game.h
@@ -1,4 +1,14 @@
 #ifndef GAME_H
 #define GAME_H
+#define BOARDSIZE 9
+
+typedef struct cell {
+    int fixed: 1;
+    int value: 4;
+} cell_t;
+
+typedef struct game_state {
+    cell_t *board[BOARDSIZE][BOARDSIZE];
+} game_state_t;
 
 #endif
