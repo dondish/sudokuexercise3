0dc395e365bf019d95a32bae44bed10f5e32bc5c
Noam Raz <noamraz8@gmail.com>
2020-01-04 19:20:45 +0200

    Clean up main

--- main.c
+++ main.c
@@ -7,10 +7,11 @@ int main(int argc, char const *argv[])
     game_state_t state;
     char line[1024];
 
-    if (argc > 0) {
+    if (argc > 1) {
         srand(atoi(argv[1]));
     } else {
-        srand(5);
+        puts("Error: missing seed argument");
+        return 1;
     }
 
     initialize(&state);
@@ -18,15 +19,14 @@ int main(int argc, char const *argv[])
     {
         if (fgets(line, 1024, stdin) == NULL) {
             if (feof(stdin)) {
-                printf("Exiting...\n");
-                exit(0);
+                puts("Exiting...");
+                return 0;
             }
-            printf("Error: fgets has failed\n");
-            exit(1);
+            puts("Error: fgets has failed");
+            return 1;
         }
         process_command(&state, line);
     }
     
     return 0;
 }
-
