2bce7b2007209469f59087506cd377c15790ebb7
Noam Raz <noamraz8@gmail.com>
2020-01-04 20:20:14 +0200

    Add generator test
    Co-authored-by: Oded Shapira <odedshapira1@mail.tau.ac.il>

--- solver_test.c
+++ solver_test.c
@@ -1,11 +1,44 @@
 #include "solver.h"
 #include "game.h"
 #include<assert.h>
+#include <stdlib.h>
 
 int main()
 {
     game_state_t game;
     int x, y;
+
+    srand(5);
+
+    generate(&game, 10);
+    assert(is_legal(&game.solution));
+    print_board(&game.board_state);
+    print_board(&game.solution);
+
+    init_board(&game.board_state);
+    game.board_state.board[0][1].fixed = 1;
+    game.board_state.board[0][1].value = 6;
+    game.board_state.board[0][8].fixed = 1;
+    game.board_state.board[0][8].value = 5;
+    game.board_state.board[2][4].value = 5;
+    game.board_state.board[2][8].fixed = 1;
+    game.board_state.board[2][8].value = 2;
+    game.board_state.board[4][7].fixed = 1;
+    game.board_state.board[4][7].value = 4;
+    game.board_state.board[6][6].value = 5;
+    game.board_state.board[8][1].fixed = 1;
+    game.board_state.board[8][1].value = 9;
+
+    assert(validate(&game));
+    assert(is_legal(&game.solution));
+
+    for (x = 0; x < BOARDSIZE; x++) {
+        for (y = 0; y < BOARDSIZE; y++) {
+            assert(game.solution.board[y][x].value > 0);
+            assert(game.solution.board[y][x].value < 10);
+        }
+    }
+    print_board(&game.solution);
 
     init_board(&game.board_state);
     init_board(&game.solution);
@@ -23,34 +56,8 @@ int main()
     game.board_state.board[7][6].value = 6;
     print_board(&game.board_state);
 
-    assert(!validate(&game));
     assert(is_legal(&game.board_state));
-    print_board(&game.board_state);
+    assert(!validate(&game));
 
-    init_board(&game.board_state);
-    game.board_state.board[0][1].fixed = 1;
-    game.board_state.board[0][1].value = 6;
-    game.board_state.board[0][8].fixed = 1;
-    game.board_state.board[0][8].value = 5;
-    game.board_state.board[2][4].value = 5;
-    game.board_state.board[2][8].fixed = 1;
-    game.board_state.board[2][8].value = 2;
-    game.board_state.board[4][7].fixed = 1;
-    game.board_state.board[4][7].value = 4;
-    game.board_state.board[6][6].value = 5;
-    game.board_state.board[8][1].fixed = 1;
-    game.board_state.board[8][1].value = 9;
-    print_board(&game.board_state);
-
-    assert(validate(&game));
-    assert(is_legal(&game.solution));
-
-    for (x = 0; x < BOARDSIZE; x++) {
-        for (y = 0; y < BOARDSIZE; y++) {
-            assert(game.solution.board[y][x].value > 0);
-            assert(game.solution.board[y][x].value < 10);
-        }
-    }
-    print_board(&game.solution);
     return 0;
 }
