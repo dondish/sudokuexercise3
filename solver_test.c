a083b18ef7b6a64f615b690f2a8a99581394fbfc
Noam Raz <noamraz8@gmail.com>
2020-01-04 18:28:50 +0200

    Update solver test to use new API

--- solver_test.c
+++ solver_test.c
@@ -4,52 +4,53 @@
 
 int main()
 {
-    board_state_t state;
-    int x,y;
+    game_state_t game;
+    int x, y;
 
-    init_board(&state);
-    state.board[0][1].fixed = 1;
-    state.board[0][1].value = 6;
-    state.board[0][8].fixed = 1;
-    state.board[0][8].value = 5;
-    state.board[1][4].fixed = 1;
-    state.board[1][4].value = 6;
-    state.board[2][8].fixed = 1;
-    state.board[2][8].value = 2;
-    state.board[3][7].fixed = 1;
-    state.board[3][7].value = 6;
-    state.board[7][6].fixed = 1;
-    state.board[7][6].value = 6;
-    print_board(&state);
+    init_board(&game.board_state);
+    init_board(&game.solution);
+    game.board_state.board[0][1].fixed = 1;
+    game.board_state.board[0][1].value = 6;
+    game.board_state.board[0][8].fixed = 1;
+    game.board_state.board[0][8].value = 5;
+    game.board_state.board[1][4].fixed = 1;
+    game.board_state.board[1][4].value = 6;
+    game.board_state.board[2][8].fixed = 1;
+    game.board_state.board[2][8].value = 2;
+    game.board_state.board[3][7].fixed = 1;
+    game.board_state.board[3][7].value = 6;
+    game.board_state.board[7][6].fixed = 1;
+    game.board_state.board[7][6].value = 6;
+    print_board(&game.board_state);
 
-    assert(!det_backtrack(&state));
-    assert(is_legal(&state));
-    print_board(&state);
+    assert(!validate(&game));
+    assert(is_legal(&game.board_state));
+    print_board(&game.board_state);
 
-    init_board(&state);
-    state.board[0][1].fixed = 1;
-    state.board[0][1].value = 6;
-    state.board[0][8].fixed = 1;
-    state.board[0][8].value = 5;
-    state.board[2][4].value = 5;
-    state.board[2][8].fixed = 1;
-    state.board[2][8].value = 2;
-    state.board[4][7].fixed = 1;
-    state.board[4][7].value = 4;
-    state.board[6][6].value = 5;
-    state.board[8][1].fixed = 1;
-    state.board[8][1].value = 9;
-    print_board(&state);
+    init_board(&game.board_state);
+    game.board_state.board[0][1].fixed = 1;
+    game.board_state.board[0][1].value = 6;
+    game.board_state.board[0][8].fixed = 1;
+    game.board_state.board[0][8].value = 5;
+    game.board_state.board[2][4].value = 5;
+    game.board_state.board[2][8].fixed = 1;
+    game.board_state.board[2][8].value = 2;
+    game.board_state.board[4][7].fixed = 1;
+    game.board_state.board[4][7].value = 4;
+    game.board_state.board[6][6].value = 5;
+    game.board_state.board[8][1].fixed = 1;
+    game.board_state.board[8][1].value = 9;
+    print_board(&game.board_state);
 
-    assert(det_backtrack(&state));
-    assert(is_legal(&state));
+    assert(validate(&game));
+    assert(is_legal(&game.solution));
 
-    for (x=0;x<BOARDSIZE;x++){
-        for (y=0;y<BOARDSIZE;y++){
-            assert(state.board[y][x].value > 0);
-            assert(state.board[y][x].value < 10);
+    for (x = 0; x < BOARDSIZE; x++) {
+        for (y = 0; y < BOARDSIZE; y++) {
+            assert(game.solution.board[y][x].value > 0);
+            assert(game.solution.board[y][x].value < 10);
         }
     }
-    print_board(&state);
+    print_board(&game.solution);
     return 0;
 }
